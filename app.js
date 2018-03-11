7fb9f78c260f7fe2abf27e8a504ae3c95238daa3
LutfiK <31626661+LutfiK@users.noreply.github.com>
2018-03-11 11:28:45 -0400

    add heroku deplyment supprt
    *updated package.json to specify node version as well as a 'start' script to be run by heroku
    *removed uncessary dependencies from app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 //importing modules
 var express = require("express");
-var mongoose = require("mongoose");
 var bodyparser = require("body-parser");
 var cors = require("cors");
 var path = require("path");
